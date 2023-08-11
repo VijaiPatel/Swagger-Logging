5ae61d0c933bd3b0de3ce17e2c3c7259494aa9e7
VijaiPatel <48962282+VijaiPatel@users.noreply.github.com>
2023-08-11 15:12:50 +0100

    Added Get and Post in values controller

--- FruityApp/Controllers/ValuesController.cs
+++ FruityApp/Controllers/ValuesController.cs
@@ -1,20 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.NetworkInformation;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.IO;
+using System.Text.Json.Serialization;
+//using FruityAppy.Controllers;
 
 namespace FruityApp.Controllers
 {
     [Route("api/[controller]")]
+    [Consumes("application/json")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
         private readonly ILogger<HomeController> _logger;
         List<string> fruitList = new List<string>
         {
-            "Apple: 24",
-            "Banana: 15",
-            "Grapes: 10"
-            };
+
+        };
+
         public ValuesController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -22,8 +30,42 @@ namespace FruityApp.Controllers
         [HttpGet]
         public List<string> Get()
         {
+            //List<string> fruitList = new List<string>();
+            string filepath = @"C:\Users\Vj\source\repos\FruityApp\FruityApp\FruitFile.txt";
+            string text = System.IO.File.ReadAllText(filepath);
+            foreach(string line in text.Split(","))
+            {
+                fruitList.Add(line);
+            }
+            _logger.LogError("Get performed sucessfully!!");
             return fruitList;
 
+        }
+        [HttpPost]
+        public string? PostValue([FromBody] string? value)
+        {
+            //List<string> fruitList = new List<string>();
+            if (value != null && value != "")
+            {
+                string filepath = @"C:\Users\Vj\source\repos\FruityApp\FruityApp\FruitFile.txt";
+                string text = System.IO.File.ReadAllText(filepath);
+                string totalText = (string)text + "," + value;
+                System.IO.File.WriteAllText(filepath, totalText);
+                foreach (string line in totalText.Split(","))
+                {
+                    fruitList.Add(line);
+                }
+            }
+            if (value != null && value != "")
+            {
+                _logger.LogInformation($" Added sucessfully : value : { value}");
+                return "Added sucessfully.";
+            }
+            else
+            {
+                _logger.LogError("Bad data action unsecessfull");
+                return "NOT ADDED, check data in right format.";
+            }
         }
     }
 }
