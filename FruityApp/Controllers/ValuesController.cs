cc93cf14c9d0084ab53f5bc5604a681223c21b1c
VijaiPatel <48962282+VijaiPatel@users.noreply.github.com>
2023-08-11 14:14:51 +0100

    Add project files.

---
+++ FruityApp/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+
+namespace FruityApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        List<string> fruitList = new List<string>
+        {
+            "Apple: 24",
+            "Banana: 15",
+            "Grapes: 10"
+            };
+        public ValuesController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public List<string> Get()
+        {
+            return fruitList;
+
+        }
+    }
+}
